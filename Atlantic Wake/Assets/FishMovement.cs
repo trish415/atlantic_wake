977b50e26d47a070106da03d907a50214ceb6a99
Trish O'Connor <pjoconnor415@gmail.com>
2015-11-11 18:25:28 -0500

    First commit of actual work
    Includes water, ship, fish, pirate ship
    fish are 'caught' when clicked on
    pirate ship is 'defeated' when clicked on 5 times

---
+++ Atlantic Wake/Assets/FishMovement.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FishMovement : MonoBehaviour {
+    public float speed; //public shows up in inspector to edit
+    public Transform target;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+        float step = speed * Time.deltaTime;
+        transform.position = Vector3.MoveTowards(transform.position, target.position, step);
+	}
+}
---
+++ Atlantic Wake/Assets/Scripts/FishMovement.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FishMovement : MonoBehaviour {
+    public float speed; //public shows up in inspector to edit
+    public Transform target;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+    
+    void OnMouseDown() {
+        Destroy(gameObject);
+    }
+	// Update is called once per frame
+	void Update () {
+        //if (Input.GetMouseButtonDown(0)){
+        //    gameObject.SetActive(false);
+        //}
+        float step = speed * Time.deltaTime;
+        transform.position = Vector3.MoveTowards(transform.position, target.position, step);
+	}
+}
+
+
