31684c7f4620c41ace09613de5f6907ef1f8db15
Trish O'Connor <pjoconnor415@gmail.com>
2015-11-26 08:59:33 -0500

    Fix hook mechanic
    Hook now works as intended-- only allowed to catch fist at specified spot,
    only allowed to place hook if it has been reeled in
    Also fixed pirate ship layering behing sailboat/fishing rod

--- Atlantic Wake/Assets/Scripts/HookCollisions.cs
+++ Atlantic Wake/Assets/Scripts/HookCollisions.cs
@@ -7,9 +7,12 @@ public class HookCollisions : MonoBehaviour {
     private float speed = 5;
     private float hookStartY = 3.5F;
     private float hookStartX = -2.23F;
+    //private BoxCollider2D boxCol = gameObject.GetComponent<BoxCollider2D>();
+    private float prevY;
 	// Use this for initialization
 	void Start () {
         transform.position = new Vector3(hookStartX, hookStartY, 0);
+        prevY = hookStartY;
 	}
 	
    void OnTriggerEnter2D (Collider2D col)
@@ -20,7 +23,6 @@ public class HookCollisions : MonoBehaviour {
             isFish.killFish();
             //send hook back to top
             newHookY = hookStartY;
-            //transform.position = new Vector3(transform.position.x, 3.75F, transform.position.z);
         }
         //add to score
         
@@ -32,14 +34,26 @@ public class HookCollisions : MonoBehaviour {
     }
 	// Update is called once per frame
 	void Update () {
-        if (Input.GetMouseButtonDown(0)){
+        //if hook has been reeled in, place where user clicks
+        if ((Input.GetMouseButtonDown(0)) && ((transform.position.y - hookStartY) > -0.1)){
             print(Input.mousePosition.y);
             if ((Input.mousePosition.x > 190F) && (Input.mousePosition.x < 250F)){
                 newHookY = 0.0197F*Input.mousePosition.y -4.5F;
             }
         }
+        
+        //if hook is moving, turn box collider off 
+        if (((prevY - transform.position.y) < 0.01) && ((prevY - transform.position.y) > -0.01)){
+            gameObject.GetComponent<BoxCollider2D>().enabled = true;
+        }
+        else{
+            gameObject.GetComponent<BoxCollider2D>().enabled = false;
+        }
+
+        //update prevY and change position of hook
+        prevY = transform.position.y;
         float step = speed*Time.deltaTime;
         transform.position = Vector2.Lerp(transform.position, new Vector3(transform.position.x, newHookY, transform.position.z), step);
-	
+
 	}
 }
