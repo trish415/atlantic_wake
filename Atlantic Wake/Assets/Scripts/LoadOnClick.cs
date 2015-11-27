d7af1c0d81e0256dbeb6a5445de91344639d7506
Trish O'Connor <pjoconnor415@gmail.com>
2015-11-27 00:49:33 -0500

    Add start screen framework
    Screens for start and instructions added.
    Need to make them look nice/actually give instructions

---
+++ Atlantic Wake/Assets/Scripts/LoadOnClick.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoadOnClick : MonoBehaviour {
+    public GameObject loadingImage;
+    public void LoadScene(int level){
+        Application.LoadLevel(level);
+        loadingImage.SetActive(true);
+    }
+}
